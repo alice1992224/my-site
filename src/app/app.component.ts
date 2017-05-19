f494884ca43c532a065b69536c5ae69967420e6d
Tzu-Yi Li <alice1992224@gmail.com>
2017-05-19 16:21:01 +0800

    change leftbar showing mechanism from angular animation to class switch.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,18 @@
 import { Component } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
-
-
-export class Leftbar {
-  state: string;
-}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('leftbarState', [
-      state('show', style({transform: 'translateX(-100%)'})),
-    ])
-  ]
 })
 
 export class AppComponent {
 
-  leftbar: Leftbar = new Leftbar();
+  isShowOnPage: boolean = true;
 
   changeLeftbarState(): void {
 
-    this.leftbar.state = (this.leftbar.state === 'show') ? 'hide' : 'show';
-    
+    this.isShowOnPage = (this.isShowOnPage) ? false : true;    
     console.log('[Info] call changeLeftbarState method in AppComponent.');
   }
 }
